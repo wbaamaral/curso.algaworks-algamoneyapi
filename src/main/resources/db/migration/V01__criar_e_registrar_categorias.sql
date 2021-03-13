876289f78e1210a93ce9b96258d7bb0f18a2aea7
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-03-13 17:27:28 -0400

    Primeira consulta a api via GET

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `categoria` (
+  `codigo` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO algamoneyapi.categoria (nome) VALUES
+	 ('Lazer'),
+	 ('Alimentação'),
+	 ('Supermercado'),
+	 ('Farmácia'),
+	 ('Outros');
