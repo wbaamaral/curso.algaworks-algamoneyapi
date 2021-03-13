f5a6616f322e9a724908893e8f1292e90147a780
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-03-13 19:04:54 -0400

    Incluindo recurso - v01

--- src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -3,8 +3,12 @@ package com.algaworks.algamoney.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algamoney.api.model.Categoria;
@@ -16,11 +20,16 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository categoriarepository;
-	
+
 	@GetMapping
-	public List<Categoria> listar(){
-		
+	public List<Categoria> listar() {
+
 		return categoriarepository.findAll();
 	}
-	
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void criar(@RequestBody Categoria categoria) {
+		categoriarepository.save(categoria);
+	}
 }
