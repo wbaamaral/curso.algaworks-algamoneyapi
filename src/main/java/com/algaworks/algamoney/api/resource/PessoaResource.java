4bd232ec48adf3bf5ae4d19855b3f0d953dd3e6f
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-03-17 20:33:40 -0400

    Implementa metodo delete pessoa com tratamento basico de erro para pessoa não encontrada, caso tente deletar mais de uma vez.

--- src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/PessoaResource.java
@@ -10,11 +10,13 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algamoney.api.event.RecursoCriadoEvent;
@@ -51,5 +53,13 @@ public class PessoaResource {
 	public Optional<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
 
 		return pessoaRepository.findById(codigo);
+	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+
+		pessoaRepository.deleteById(codigo);
+
 	}
 }
