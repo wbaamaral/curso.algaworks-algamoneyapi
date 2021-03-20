182afd61d7bc5668bddb7486b78151e43d768fa8
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-03-20 00:41:19 -0400

    atualização de propriedade individual(ativo?) do recurso de pessoa

--- src/main/java/com/algaworks/algamoney/api/service/PessoaService.java
+++ src/main/java/com/algaworks/algamoney/api/service/PessoaService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 import com.algaworks.algamoney.api.model.Pessoa;
 import com.algaworks.algamoney.api.repository.PessoaRepository;
 
-
 @Service
 public class PessoaService {
 
@@ -16,8 +15,7 @@ public class PessoaService {
 	PessoaRepository pessoaRepository;
 
 	public Pessoa atualizar(Long codigo, Pessoa pessoa) {
-		Pessoa pessoaSalva = this.pessoaRepository.findById(codigo)
-				.orElseThrow(() -> new EmptyResultDataAccessException(1));
+		Pessoa pessoaSalva = buscarPessoaPorCodigo(codigo);
 
 		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
 
@@ -26,4 +24,18 @@ public class PessoaService {
 		return pessoaSalva;
 	}
 
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+
+		Pessoa pessoaSalva = buscarPessoaPorCodigo(codigo);
+
+		pessoaSalva.setAtivo(ativo);
+
+		pessoaRepository.save(pessoaSalva);
+	}
+
+	private Pessoa buscarPessoaPorCodigo(Long codigo) {
+		Pessoa pessoaSalva = this.pessoaRepository.findById(codigo)
+				.orElseThrow(() -> new EmptyResultDataAccessException(1));
+		return pessoaSalva;
+	}
 }
