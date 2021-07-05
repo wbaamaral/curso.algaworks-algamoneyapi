03c555f23deba8a5c1050900312a384906846be0
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-07-04 20:52:46 -0400

    5.4. Validando inconsistências

--- src/main/java/com/algaworks/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/algaworks/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -77,6 +79,20 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		}
 
 		return erros;
+	}
+
+	@ExceptionHandler({ DataIntegrityViolationException.class })
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex,
+			WebRequest request) {
+
+		String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null,
+				LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+
 	}
 
 	public static class Erro {
