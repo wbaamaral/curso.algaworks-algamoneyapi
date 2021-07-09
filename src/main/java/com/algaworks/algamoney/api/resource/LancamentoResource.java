9a38f86ba56af03f525a76f82b8c78facce8e76a
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-07-08 21:48:50 -0400

    5.8. Desafio: Removendo lançamentos

--- src/main/java/com/algaworks/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/LancamentoResource.java
@@ -13,6 +13,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -88,4 +89,9 @@ public class LancamentoResource {
 		return ResponseEntity.badRequest().body(erros);
 	}
 
+	@DeleteMapping("/{codigo}")
+	public void remover(@PathVariable Long codigo) {
+
+		lancamentoRepository.deleteById(codigo);
+	}
 }
