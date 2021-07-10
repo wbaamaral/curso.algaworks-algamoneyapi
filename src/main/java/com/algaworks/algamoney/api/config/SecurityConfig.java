2be4ad5d8ae70b040fb9cf761a1b1609b18c9295
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-07-10 17:05:57 -0400

    Revisão aula 6.1

--- src/main/java/com/algaworks/algamoney/api/config/SecurityConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/SecurityConfig.java
@@ -13,15 +13,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ROLE");
+		auth.inMemoryAuthentication()
+			.withUser("admin")
+			.password("{noop}admin").roles("ROLE");
 
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/categorias").permitAll().anyRequest().authenticated().and().httpBasic()
-				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf()
-				.disable();
+		http.authorizeRequests()
+			.antMatchers("/categorias").permitAll()
+			.anyRequest().authenticated().and()
+			.httpBasic().and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.csrf().disable();
 	}
 
 }
