2638f32988169319405a5ae908058d839d504ad9
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-07-14 22:12:20 -0400

    6.6. Renovando o access token com o refresh token

--- src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
@@ -27,8 +27,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.withClient("angular")
 			.secret("@ngul@r0")
 			.scopes("read", "write")
-			.authorizedGrantTypes("password")
-			.accessTokenValiditySeconds(120);
+			.authorizedGrantTypes("password", "refresh_token")
+			.accessTokenValiditySeconds(20)
+			.refreshTokenValiditySeconds(3600 * 24);
 
 	}
 	
@@ -37,6 +38,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		endpoints
 			.tokenStore(tokenStore())
 			.accessTokenConverter(accessTokenConverter())
+			.reuseRefreshTokens(false)
 			.authenticationManager(authenticationManager);
 
 	}
