67c0c003ab046ffc8fa8e2d52e5382817209d91b
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-07-15 22:15:28 -0400

    6.9. O que é CORS? - Teste temporário para saber que existe.

--- src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resource/CategoriaResource.java
@@ -11,6 +11,7 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,6 +40,7 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService categoriaService;
 
+	@CrossOrigin (maxAge = 10, origins = { "http://localhost:9800"})
 	@GetMapping
 	public List<Categoria> listar() {
 
