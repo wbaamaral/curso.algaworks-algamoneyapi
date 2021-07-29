4e04368d099091362084a5ccb0cf036db3e29cfa
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-07-29 18:14:35 -0400

    7.1. Implementando projeção de lançamento

---
+++ src/main/java/com/algaworks/algamoney/api/repository/projection/ResumoLancamento.java
@@ -0,0 +1,32 @@
+package com.algaworks.algamoney.api.repository.projection;
+
+import java.time.LocalDate;
+
+import com.algaworks.algamoney.api.model.TipoLancamento;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor
+public class ResumoLancamento {
+
+	@EqualsAndHashCode.Include
+	private Long codigo;
+
+	private String descricao;
+
+	private LocalDate dataVencimento;
+
+	private LocalDate dataPagamento;
+
+	private TipoLancamento tipo;
+
+	private String categoria;
+
+	private String pessoa;
+
+	
+}
