43f4078e98cafea8c3053420541a3d74cec9575a
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2021-08-11 13:18:39 -0400

    Ajuste - script migracao banco de dados - deploy

--- src/main/resources/db/migration/V04__criar_tabelas_usuario_permissoes_e_grupos.sql
+++ src/main/resources/db/migration/V04__criar_tabelas_usuario_permissoes_e_grupos.sql
@@ -1,4 +1,30 @@
--- use algamoneyapi;
+--  use algamoneyapi;
+
+DROP TABLE IF EXISTS `usuario`;
+DROP TABLE IF EXISTS `permissoes`;
+DROP TABLE IF EXISTS `permissao`;
+DROP TABLE IF exists `usuario_permissao`;
+
+CREATE TABLE usuario (
+	codigo BIGINT(20) PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	senha VARCHAR(150) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE permissao (
+	codigo BIGINT(20) PRIMARY KEY,
+	descricao VARCHAR(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE usuario_permissao (
+	codigo_usuario BIGINT(20) NOT NULL,
+	codigo_permissao BIGINT(20) NOT NULL,
+	PRIMARY KEY (codigo_usuario, codigo_permissao),
+	FOREIGN KEY (codigo_usuario) REFERENCES usuario(codigo),
+	FOREIGN KEY (codigo_permissao) REFERENCES permissao(codigo)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 -- Insert usuarios
 INSERT INTO usuario (codigo, nome, email, senha) values (1, 'Administrador', 'admin@algamoney.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
